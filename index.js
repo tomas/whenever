ded072f0101a8f8f940839621c21eff4fd55e641
Tomás Pollak <tomaspollak@gmail.com>
2015-08-19 17:36:08 -0700

    Filter directories.

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-var ls       = require('fs').readdirSync,
-    basename = require('path').basename,
-    resolve  = require('path').resolve;
+var ls   = require('fs').readdirSync,
+    stat = require('fs').statSync,
+    path = require('path');
 
-module.exports = function(keys, path) {
+module.exports = function(keys, root) {
   var obj = { _keys: [] };
 
   if (keys === '*') {
-    keys = ls(path);
+    keys = ls(root).filter(function(file) {
+      return !stat(path.join(root, file)).isDirectory()
+    });
   } else if (typeof keys == 'string') {
     keys = [keys]
   }
 
   keys.forEach(function(mod) {
-    var key = basename(mod).replace(/\.js$/, '');
+    var key = path.basename(mod).replace(/\.js$/, '');
     obj._keys.push(key);
 
     Object.defineProperty(obj, key, {
       get: function() {
-        var target = path ? resolve(path, mod) : mod;
+        var target = root ? path.resolve(root, mod) : mod;
         return require(target);
       }
     });
